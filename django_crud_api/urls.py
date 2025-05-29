28663b51c215a6c19b696bc00df418147df9b74f
juan david delgado caicedo <11.juancaicedodavid@gmail.com>
2025-05-29 16:30:42 -0500

    urls

--- django_crud_api/urls.py
+++ django_crud_api/urls.py
@@ -52,4 +52,4 @@ urlpatterns = [
     # Rutas para la documentación de la API
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Para servir archivos estáticos y medios en el servidor de desarrollo
+] 
